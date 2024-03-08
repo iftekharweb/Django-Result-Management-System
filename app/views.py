cb3ab75ae2ce06eee7139ca5f2960179f30e96d0
iftekharweb <iftekharweb@gmail.com>
2024-03-08 19:29:20 +0600

    change in app>views.py

--- app/views.py
+++ app/views.py
@@ -2,10 +2,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import render
 
-
+@api_view(['GET'])
 def get_students(request):
-    return render(request, 'hello.html')
+    return Response('OK')
 
-
+@api_view(['GET'])
 def test_home(request):
     return render(request,'test.html')
