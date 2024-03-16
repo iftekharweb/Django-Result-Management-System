f375a26ed9f27e7462f89f538fc1f42e9104e363
iftekharweb <iftekharweb@gmail.com>
2024-03-16 17:21:12 +0600

    admin.py update

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,44 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Student)
-admin.site.register(models.Teacher)
-admin.site.register(models.Course)
-admin.site.register(models.Semester)
-# Register your models here.
+
+@admin.register(models.Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = [
+        'id_no',
+        'name',
+        'email'
+    ]
+    search_fields = ['id_no', 'name']
+
+@admin.register(models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = [
+        'course_code',
+        'title',
+        'semester',
+        'sectionA_assigned_teacher',
+        'sectionB_assigned_teacher',
+        'credits'
+    ]
+    search_fields = ['course_code']
+
+@admin.register(models.Semester)
+class SemesterAdmin(admin.ModelAdmin):
+    list_display = [
+        'semester_no',
+        'title'
+    ]
+    search_fields = ['semester_no','title']
+
+@admin.register(models.Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = [
+        'id_no',
+        'name',
+        'semester',
+        'session'
+    ]
+    list_editable = ['semester']
+    search_fields = ['id_no', 'semester', 'session']
+
